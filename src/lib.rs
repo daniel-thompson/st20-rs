7a78b5b78513e191dd49c699496ec89a4df36b2b
Daniel Thompson <daniel@redfelineninja.org.uk>
2022-07-24 13:35:24 +0100

    lib: Refactor into main and library
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2022 Daniel Thompson
+
+#![allow(non_snake_case)]
+
+use std::arch::global_asm;
+use std::os::raw::{c_int, c_void};
+use std::ptr;
+
+#[repr(C)]
+#[derive(Debug, Default, Copy, Clone)]
+pub struct st20c1_regs {
+    pub Areg: u32,
+    pub Breg: u32,
+    pub Creg: u32,
+    pub Iptr: u32,
+    pub Status: u32,
+    pub Wptr: u32,
+    pub Tdesc: u32,
+    pub IOreg: u32,
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct st20c1_ctx {
+    pub regs: st20c1_regs,
+    pub mem: *mut c_void,
+}
+
+impl Default for st20c1_ctx {
+    fn default() -> Self {
+        Self {
+            regs: st20c1_regs::default(),
+            mem: ptr::null_mut(),
+        }
+    }
+}
+
+extern "C" {
+    pub fn st20c1_run(ctx: *mut st20c1_ctx) -> c_int;
+}
+
+global_asm!(include_str!(concat!(env!("OUT_DIR"), "/c1core-t32.s")));
+
+#[macro_export]
+macro_rules! assert_regs {
+    ($core:expr, $a:expr) => {
+        assert_eq!($core.regs.Areg, $a);
+    };
+    ($core:expr, $a:expr, $b:expr) => {
+        assert_eq!($core.regs.Areg, $a);
+        assert_eq!($core.regs.Breg, $b);
+    };
+    ($core:expr, $a:expr, $b:expr, $c:expr) => {
+        assert_eq!($core.regs.Areg, $a);
+        assert_eq!($core.regs.Breg, $b);
+        assert_eq!($core.regs.Creg, $c);
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn run_fragment(code: &mut [u8]) -> st20c1_ctx {
+        let mut c1 = st20c1_ctx::default();
+        c1.mem = code.as_mut_ptr() as *mut c_void;
+
+        let res;
+        unsafe {
+            res = st20c1_run(&mut c1);
+        }
+
+        // a "fragment" exists from the final instruction in the memory,
+        // which must be a breakpoint instruction
+        assert_eq!(res, 0);
+        assert_eq!(c1.regs.Iptr, code.len() as u32);
+
+        c1
+    }
+
+    #[test]
+    fn test_ldc4bit() {
+        let c1 = run_fragment(&mut vec![0x4c, 0x4b, 0x4a, 0xff]);
+        assert_regs!(c1, 0x0a, 0x0b, 0x0c);
+    }
+
+    #[test]
+    fn test_ldc8bit() {
+        let c1 = run_fragment(&mut vec![0x2c, 0x40, 0x2b, 0x41, 0x2a, 0x42, 0xff]);
+        assert_regs!(c1, 0xa2, 0xb1, 0xc0);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,18 @@
-#![allow(non_snake_case)]
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2022 Daniel Thompson
 
-use std::arch::global_asm;
-use std::os::raw::{c_int, c_void};
-
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct st20c1_ctx {
-    pub Areg: u32,
-    pub Breg: u32,
-    pub Creg: u32,
-    pub Iptr: u32,
-    pub Status: u32,
-    pub Wptr: u32,
-    pub Tdesc: u32,
-    pub IOreg: u32,
-    pub mem: *mut c_void,
-}
-
-extern "C" {
-    pub fn st20c1_run(ctx: *mut st20c1_ctx) -> c_int;
-}
-
-global_asm!(include_str!(concat!(env!("OUT_DIR"), "/c1core-t32.s")));
+use st20_rs::*;
+use std::os::raw::c_void;
 
 fn main() {
-    let mut ctx = st20c1_ctx {
-        Areg: 0xaaaaaaaa,
-        Breg: 0xbbbbbbbb,
-        Creg: 0xcccccccc,
-        Iptr: 0,
-        Status: 0,
-        Wptr: 0,
-        Tdesc: 0,
-        IOreg: 0,
-        mem: std::ptr::null_mut(),
-    };
-
+    let mut c1 = st20c1_ctx::default();
     let mut code = vec![1_u8];
-    ctx.mem = code.as_mut_ptr() as *mut c_void;
+    c1.mem = code.as_mut_ptr() as *mut c_void;
 
     let res;
     unsafe {
-        res = st20c1_run(&mut ctx);
+        res = st20c1_run(&mut c1);
     }
 
-    println!("{ctx:#?} -> {res}");
+    println!("{c1:#?} -> {res}");
 }
